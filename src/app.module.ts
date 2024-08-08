dcafa976a78beb4692dc2b936d4f21e0c06b9e34
Luiz Ribeiro <luizribeirodesenvolvedor@gmail.com>
2024-08-08 15:26:41 -0300

    Deploy do projeto do brecho

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,25 +7,23 @@ import { ProdutoModule } from './produto/produto.module';
 import { AuthModule } from './auth/auth.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_brecho',
-      entities: [Categoria,Produto,Usuario],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+        imports: [ConfigModule],
     }),
     CategoriaModule,
     ProdutoModule,
     AuthModule,
     UsuarioModule
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -3,36 +3,44 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } f
 import { Categoria } from "../../categoria/entities/categoria.entity";
 import { Transform, TransformFnParams } from "class-transformer";
 import { Usuario } from "../../usuario/entities/usuario.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({name: "tb_produtos"})
 export class Produto {
 
+    @ApiProperty()
     @PrimaryGeneratedColumn()
     id: number;
     
     /*@Transform(({ value }: TransformFnParams) => value?.trim())*/
+    @ApiProperty()
     @IsNotEmpty()
     @Column({length: 100, nullable: false})
     nome: string
 
+    @ApiProperty()
     @IsNotEmpty()
     @Column({type: "varchar", length: 4, nullable: false})
     tamanho: string
 
+    @ApiProperty()
     @IsNotEmpty()
     @Column({length: 2000, nullable: false})
     foto: string
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsNumber({maxDecimalPlaces: 2})
     @Column({ type: "decimal", precision: 10, scale: 2 })
     preco: number
     
+    @ApiProperty({ type: () => Categoria})
     @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
         onDelete: "CASCADE"
     })
     categoria: Categoria
 
+    @ApiProperty({ type: () => Usuario})
     @ManyToOne(() => Usuario, (usuario) => usuario.produto, {
         onDelete: "CASCADE"
     })
